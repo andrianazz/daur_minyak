5d04c0a7862d045ddff7a79aa2f7768b52006d49
andrianazz <andrianwahyu41@gmail.com>
2023-10-09 20:14:38 +0700

    fix: remove unused

--- lib/presentation/widgets/carousel_widget.dart
+++ lib/presentation/widgets/carousel_widget.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:daur_minyak/presentation/misc/constants.dart';
 import 'package:daur_minyak/presentation/misc/methods.dart';
