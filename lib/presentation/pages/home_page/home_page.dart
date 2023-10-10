598125b9802ff7d28135368c5b4b8ceb7acd21f0
andrianazz <andrianwahyu41@gmail.com>
2023-10-10 18:39:59 +0700

    add: pesan masuk done

--- lib/presentation/pages/home_page/home_page.dart
+++ lib/presentation/pages/home_page/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:daur_minyak/presentation/extensions/build_context_extensions.dart';
 import 'package:daur_minyak/presentation/misc/constants.dart';
 import 'package:daur_minyak/presentation/pages/home_page/beranda_page.dart/beranda_page.dart';
+import 'package:daur_minyak/presentation/pages/home_page/pesan_masuk_page/pesan_masuk_page.dart';
 import 'package:daur_minyak/presentation/pages/home_page/pesanan_page/pesanan_page.dart';
 import 'package:daur_minyak/presentation/providers/router/router_provider.dart';
 import 'package:daur_minyak/presentation/providers/user_data/user_data_provider.dart';
@@ -42,7 +43,7 @@ class _HomePageState extends ConsumerState<HomePage> {
           const BerandaPage(),
           const PesananPage(),
           Container(),
-          Container(),
+          const PesanMasukPage(),
           Container(),
         ],
       ),
